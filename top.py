efbd894929ab3ed4e3a2fe1a4541fa83fc768542
MedoX67 <93802008+MedoX67@users.noreply.github.com>
2021-11-26 23:04:29 +0200

    Add files via upload

---
+++ top.py
@@ -0,0 +1,96 @@
+import os
+os.system ('clear')
+ 
+K7x = 200099
+
+print (K7x)
+
+v7x = ("Hello World")
+print (v7x)
+
+flo = 34537.99303
+
+print (flo)
+
+vhx = input ("Type Your Name: ")
+
+print ("Your Name Is: " +vhx)
+
+hyx = input ("Input Your Password: ")
+
+print ("Your Password Is: " +hyx)
+num_one = 1000
+
+num_two = 20
+
+num_three = 2
+
+print (num_one*num_two/num_three)
+
+v7c = ("hello world")
+print (v7c *1000)
+
+print (len (v7c))
+
+print (v7c [8])
+k7x = ('❤️❤️❤️😖👿👿👺👹👺👿👹👺👹👺👹👿👹👿👺👹👺👹👺👹👿👹👺')
+
+print (k7x *10)
+
+name = input ("Your Name: ")
+
+if name == "Medo" :
+   print ("Nice Name...!")
+   print ("thank you")
+
+else:
+    print ("faild")
+    print ("Type 'v7x' please.....!")
+    input ("=====>>>>: ")
+    name = "v7x"
+    if name == 'v7x' :
+       print ('Ok... Done..!')
+       print ('Welcome To K679 Tool...!🤗🤗')
+#التكرار وعوامل المقارنة
+v = 0
+while v < 1001091 :
+     print (v *5)
+     v=v+1
+for v in range (10000):
+    print ('01010101' *5)
+t7x = ['King',"Medo",'Asar',"yazen"]
+t7x.append ('6788')
+t7x [1]="end"
+print (t7x[0]*578+t7x[2]*78+t7x[1]*71)
+
+G7x = [1,2,3,4,5,6,7,8]
+print (G7x*6788)
+                      #القواميس.     
+
+H7x={'key':"value",'key1':"value1"}
+print (H7x['key1'])
+
+#مثال           
+users={'user':'10000',"user1":"56678",'user2':'7878'}
+print (users["user1"])
+users["user1"]=6000000
+print (users["user1"])
+#الدوال                 
+def h7x () :
+    print (400*400)
+h7x ()  
+    #القيمة المسترجعة               
+def y7x (user,user1) :
+    return user +user1
+v=y7x(10000,2000)
+print (v)  
+
+#مثال للتوضيح             
+def k7x (your_name) :
+    print ("welcome back", your_name)
+
+k7x ("Medo")
+# و الدوال               
+
+import list99900099999
+list99
